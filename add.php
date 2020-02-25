9e4257cba33b96b263bcf79be45d592dfda12b05
anita stha <cixwareanita.com>
2020-02-25 14:09:11 +0545

    operator is done

---
+++ add.php
@@ -0,0 +1,12 @@
+<?php
+$num1=4;
+$num2=2;
+$add=$num1+$num2;
+$sub=$num1-$num2;
+$multiply=$num1*$num2;
+$divide=$num1/$num2;
+echo "Add = ".$add;
+echo"sub = ".$sub;
+echo"multi = ".$multiply;
+echo"divide = ".$divide;
+?>
