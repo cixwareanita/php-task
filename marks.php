f3ad83105620459d71bb4f4bed571c7dd5ff16c8
anita stha <cixwareanita.com>
2020-02-25 14:33:34 +0545

    8 sub marks is calculated

---
+++ marks.php
@@ -0,0 +1,15 @@
+<?php
+$english= 80;
+$math=50;
+$social= 86;
+$optmath= 70;
+$moral= 85;
+$EHP=70;
+$nepali= 60;
+$computer= 80;
+$sumOfSubject =$math+$english+$nepali+$social+$computer+$moral+$optmath+$majeng;
+echo "Sumof8sub: ",$sumOfSubject;
+$percent=$sumOfSubject/8;
+echo "\n";
+echo "percent: ".$percent ."%";
+?>
