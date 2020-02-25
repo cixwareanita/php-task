7b16e282475dd02513aa9aa116a54a7acccb3aad
anita stha <cixwareanita.com>
2020-02-25 22:40:37 +0545

    greaterb among 3 number

---
+++ greater.php
@@ -0,0 +1,15 @@
+<?php
+$num1=10;
+$num2=8;
+$num3=7;
+if($num1>$num2 && $num1>$num2){
+        echo "num1 is greater";
+}
+if($num2>$num1 && $num2>$num3){
+    echo "num2 is greater";
+}
+if($num3>$num1 && $num3>$num1){
+    echo "num3 is greater";
+}
+
+?>
