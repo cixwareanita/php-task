6016b7bd6e23fedb4899b6aff34d9cc23f70ebf8
anita stha <cixwareanita.com>
2020-02-26 16:43:59 +0545

    given num is prime or not

---
+++ prime.php
@@ -0,0 +1,16 @@
+<?php
+    $number =4;
+    
+    if($number>3){
+        if($number%2==0 || $number%3==0){
+        echo "$number is not a prime";
+        }    
+        else{
+        echo "$number is prime number";
+        }
+    }    
+    else{
+        echo "$number is prime";
+    }
+    
+?>
