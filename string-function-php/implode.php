e1e532d2f7375dd0224cdbdf508c5f23ee6fa9ed
anita stha <cixwareanita.com>
2020-03-02 13:51:05 +0545

    array to string

---
+++ string-function-php/implode.php
@@ -0,0 +1,14 @@
+html>
+<body>
+
+<?php
+$arr=["anita","ashma","alisha"];
+echo implode(" ",$arr) "<";//seperates the array elements with space.
+echo implode("+",$arr) ."<br>";//seperates the array element with + sign.
+echo implode(" ? ",$arr) ."<br>";//seperates the array elements with ? sign.
+
+?> 
+ 
+ 
+</body>
+</html>
