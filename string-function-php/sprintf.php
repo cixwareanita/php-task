ed29d9b17bd994c25fc6c5a010a114060db1f759
anita stha <cixwareanita.com>
2020-03-03 21:55:53 +0545

    print as given in % sign

---
+++ string-function-php/sprintf.php
@@ -0,0 +1,8 @@
+<?php
+$str1 = 84974;
+echo sprintf("%%b = %b",$str1)."<br>"; // Binary number
+echo sprintf("%%c = %c",$str1)."<br>"; // The ASCII Character
+echo sprintf("%%o = %o",$str1)."<br>"; // Octal number
+echo sprintf("%%s = %s",$str1)."<br>"; // String
+
+?>
